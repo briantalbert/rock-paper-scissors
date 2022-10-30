b263854a31cc18f7f94e9af19ec9697651055243
briantalbert <brianltalbert@gmail.com>
2022-10-30 09:07:19 -0400

    Initial commit

---
+++ game.js
@@ -0,0 +1,6 @@
+function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"]
+    return choices[Math.floor(Math.random() * choices.length)]
+}
+
+console.log(getComputerChoice())
