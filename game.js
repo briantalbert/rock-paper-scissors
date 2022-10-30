2a59e5901ef61f4f37725a51b7e2bf2ec3e059af
briantalbert <brianltalbert@gmail.com>
2022-10-30 14:47:19 -0400

    Finished game for play in console

--- game.js
+++ game.js
@@ -1,6 +1,48 @@
-function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"]
-    return choices[Math.floor(Math.random() * choices.length)]
+let choices = ["rock", "paper", "scissors"]
+
+function playRound() {
+    let compIdx = choices.indexOf(getComputerChoice())
+    let playerIdx = choices.indexOf(getPlayerChoice())
+
+    if (compIdx == playerIdx) {
+        //It's a tie
+        console.log('Computer choice: ' +  choices[compIdx])
+        console.log('Player choice: ' + choices[playerIdx])
+        console.log("It's a tie!")
+    }
+    else if ((playerIdx + 1) % 3 == compIdx) {
+        //Computer wins
+        console.log('Computer choice: ' +  choices[compIdx])
+        console.log('Player choice: ' + choices[playerIdx])
+        console.log("Computer wins!")
+    }
+
+    else {
+        //Player wins
+        console.log('Computer choice: ' +  choices[compIdx])
+        console.log('Player choice: ' + choices[playerIdx])
+        console.log("Player wins!")
+    }
+
 }
 
-console.log(getComputerChoice())
+function getComputerChoice() {
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    return computerChoice;
+}
+
+function getPlayerChoice() {
+    let playerChoice = ""
+    while (!choices.includes(playerChoice)) {
+        playerChoice = prompt("Enter your choice: Rock, Paper, or Scissors.")
+        playerChoice = playerChoice.toLowerCase()
+    }
+
+    return playerChoice;
+}
+
+//console.log(getPlayerChoice())
+
+
+
+playRound()
