f30eea3f07267c6f2d00feafbc0bf7978595b8cb
Brian Talbert <BrianLTalbert@gmail.com>
2022-11-02 10:41:17 -0400

    Remove extra function

--- game.js
+++ game.js
@@ -23,18 +23,13 @@ function playRound(choice) {
         document.querySelector(".winner").textContent = "It's a Tie!"
         scoresArray[2] += 1
         document.querySelector(".scoreboard .draw .score").textContent = scoresArray[2]
-        console.log('Computer choice: ' +  choices[compIdx])
-        console.log('Player choice: ' + choices[playerIdx])
-        console.log("It's a tie!")
+
     }
     else if ((playerIdx + 1) % 3 == compIdx) {
         //Computer wins
         document.querySelector(".winner").textContent = "Computer wins!"
         scoresArray[1] += 1
         document.querySelector(".scoreboard .computer .score").textContent = scoresArray[1]
-        console.log('Computer choice: ' +  choices[compIdx])
-        console.log('Player choice: ' + choices[playerIdx])
-        console.log("Computer wins!")
     }
 
     else {
@@ -42,9 +37,6 @@ function playRound(choice) {
         document.querySelector(".winner").textContent = "You win!"
         scoresArray[0] += 1
         document.querySelector(".scoreboard .player .score").textContent =scoresArray[0]
-        console.log('Computer choice: ' +  choices[compIdx])
-        console.log('Player choice: ' + choices[playerIdx])
-        console.log("Player wins!")
     }
 
 }
@@ -53,14 +45,3 @@ function getComputerChoice() {
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
 }
-
-// function getPlayerChoice() {
-//     let playerChoice = ""
-//     while (!choices.includes(playerChoice)) {
-//         playerChoice = prompt("Enter your choice: Rock, Paper, or Scissors.")
-//         playerChoice = playerChoice.toLowerCase()
-//     }
-
-//     return playerChoice;
-// }
-
