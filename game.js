822c1cf5c7ba9cc3f9dcc6e08eb41a34895b2e71
Brian Talbert <BrianLTalbert@gmail.com>
2022-11-02 10:34:43 -0400

    Add UI to game

--- game.js
+++ game.js
@@ -1,17 +1,36 @@
-let choices = ["rock", "paper", "scissors"]
+let choices = ["Rock", "Paper", "Scissors"]
+let scoresArray = [0, 0, 0]
 
-function playRound() {
+//initializing scoreboard
+const scores = document.querySelectorAll(".score")
+scores.forEach(score => score.textContent = 0)
+document.querySelector(".winner").textContent = "Press a button above to play!"
+
+const choiceButtons = document.querySelectorAll(".choice")
+choiceButtons.forEach(choice => choice.addEventListener('click', sendChoice => {
+    let pick = choice.id.charAt(0).toUpperCase() + choice.id.slice(1)
+    playRound(pick)
+} ))
+
+function playRound(choice) {
     let compIdx = choices.indexOf(getComputerChoice())
-    let playerIdx = choices.indexOf(getPlayerChoice())
+    let playerIdx = choices.indexOf(choice)
+    document.querySelector(".round").textContent = `${choices[playerIdx]} vs ${choices[compIdx]}`
 
     if (compIdx == playerIdx) {
         //It's a tie
+        document.querySelector(".winner").textContent = "It's a Tie!"
+        scoresArray[2] += 1
+        document.querySelector(".scoreboard .draw .score").textContent = scoresArray[2]
         console.log('Computer choice: ' +  choices[compIdx])
         console.log('Player choice: ' + choices[playerIdx])
         console.log("It's a tie!")
     }
     else if ((playerIdx + 1) % 3 == compIdx) {
         //Computer wins
+        document.querySelector(".winner").textContent = "Computer wins!"
+        scoresArray[1] += 1
+        document.querySelector(".scoreboard .computer .score").textContent = scoresArray[1]
         console.log('Computer choice: ' +  choices[compIdx])
         console.log('Player choice: ' + choices[playerIdx])
         console.log("Computer wins!")
@@ -19,6 +38,9 @@ function playRound() {
 
     else {
         //Player wins
+        document.querySelector(".winner").textContent = "You win!"
+        scoresArray[0] += 1
+        document.querySelector(".scoreboard .player .score").textContent =scoresArray[0]
         console.log('Computer choice: ' +  choices[compIdx])
         console.log('Player choice: ' + choices[playerIdx])
         console.log("Player wins!")
@@ -31,18 +53,13 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function getPlayerChoice() {
-    let playerChoice = ""
-    while (!choices.includes(playerChoice)) {
-        playerChoice = prompt("Enter your choice: Rock, Paper, or Scissors.")
-        playerChoice = playerChoice.toLowerCase()
-    }
+// function getPlayerChoice() {
+//     let playerChoice = ""
+//     while (!choices.includes(playerChoice)) {
+//         playerChoice = prompt("Enter your choice: Rock, Paper, or Scissors.")
+//         playerChoice = playerChoice.toLowerCase()
+//     }
 
-    return playerChoice;
-}
+//     return playerChoice;
+// }
 
-//console.log(getPlayerChoice())
-
-
-
-playRound()
